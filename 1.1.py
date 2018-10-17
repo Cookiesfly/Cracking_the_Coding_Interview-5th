ad7538d4dbce90489a672058f1fb2f78b6d14ba7
Cookiesfly <zheng.fei42@gmail.com>
2018-10-17 22:09:25 +0800

    begin

---
+++ 1.1.py
@@ -0,0 +1,13 @@
+#实现一个算法，确定一个字符串的所有字符是否全都不同。假使不允许使用额外的数据结构，又该如何处理？
+#Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures?
+
+# -*- coding:utf-8 -*-
+def checkDifferent1(self, iniString):
+    return len(set(iniString)) == len(iniString)
+
+
+def checkDifferent2(self, iniString):
+    for i in iniString:
+        if (iniString.count(i)>1):
+            return False
+    return True
